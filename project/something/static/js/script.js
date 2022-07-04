19a6699494d2ff8e1941bdd5e65c3194dcdcf746
Igor_Chebotar <igorcebotar5@gmail.com>
2022-07-04 23:08:31 +0300

    Fix some bugs

--- project/something/static/js/script.js
+++ project/something/static/js/script.js
@@ -1,6 +1,6 @@
 
-function CopyURL() {
-  var text = document.getElementById('url_hash')
+function CopyURL(id) {
+  var text = document.getElementById('url_hash_' + id)
   text.select();
   document.execCommand('copy')
 }
